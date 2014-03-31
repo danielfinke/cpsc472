89a5197f1a1eb2c2045cb1d7a07dfe41c37d3382
Andrew J Toms II <toms@unbc.ca>
2014-03-31 14:22:23 -0700

    Basically just what I just said, but now I actually added my new files.

---
+++ src/ca/unbc/cpsc472/mynextphone/models/SliderListAdapter.java
@@ -0,0 +1,50 @@
+package ca.unbc.cpsc472.mynextphone.models;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class SliderListAdapter extends BaseAdapter {
+
+	private ArrayList<QuestionAnswer> theItems;
+	private Context context;
+	private View view;
+	
+	public SliderListAdapter(Context c, ArrayList<QuestionAnswer> theItems){
+		this.theItems = theItems;
+		this.context = c;
+		this.view = null;
+	}
+	
+	@Override
+	public int getCount() {
+		return theItems.size();
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public QuestionAnswer getAnswer(int i){
+		return theItems.get(i);
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int arg0, View arg1, ViewGroup arg2) {
+		if(view == null)
+			view = theItems.get(arg0).getView(context); 
+		return view;
+	}
+
+}
